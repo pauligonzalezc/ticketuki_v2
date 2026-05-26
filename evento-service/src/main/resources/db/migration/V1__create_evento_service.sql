c5d43c54a004494dd675a78bc43ccf0280e7e844
xelomartabit <mmartabit182@gmail.com>
2026-05-26 03:54:20 -0400

    add_micro_evento
    agrege el microservicio de evento

---
+++ evento-service/src/main/resources/db/migration/V1__create_evento_service.sql
@@ -0,0 +1,17 @@
+CREATE TABLE estado_evento (
+    id_estado_evento BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre_estado_evento VARCHAR(25)
+);
+
+CREATE TABLE evento (
+    id_evento BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre_evento VARCHAR(100) NOT NULL,
+    aforo_evento INTEGER NOT NULL,
+    fecha_evento DATE NOT NULL,
+    descripcion VARCHAR(500) NOT NULL,
+    estado_evento_id_estado BIGINT,
+    recinto_id_recinto BIGINT,
+    INDEX idx_nombre (nombre_evento),
+    INDEX idx_fecha (fecha_evento),
+    FOREIGN KEY (estado_evento_id_estado) REFERENCES estado_evento(id_estado_evento)
+);
